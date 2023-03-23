4bacc0a8d9882e50c755ce8aa727616992b906c9
muhfarrasm <muhammadfarrasm@gmail.com>
2023-03-23 10:42:46 +0700

    Prosedur Input

--- Insertion sort/Insertion sort.cpp
+++ Insertion sort/Insertion sort.cpp
@@ -1,20 +1,31 @@
-// Insertion sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int arr[20];															// Membuat Array dengan panjang data 20
+int n;																	// Membuat Variable Inputan n
+
+void input() {					// procedure input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";					// Membuat inputan jumlah element array
+		cin >> n;														// Memanggil variable inputan n
+		
+		if (n <= 20) {													// Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; //Menampilkan Pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;															// Membuat jarak per baris program
+	cout << "================================================" << endl;		// Membuat tampilan susunan data element array
+	cout << "===========Masukkan Element Array===============" << endl;
+	cout << "================================================" << endl;
+
+	for (int i = 0; i < n; i++)										// Menggunakan perulangan for untuk menyimpan  data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";						
+		cin >> arr[i];												// Menyimpan nilai data n kedalam array arr
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
