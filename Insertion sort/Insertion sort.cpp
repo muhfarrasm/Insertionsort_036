8255edf0e6438e769e4fc7eb9d15f9db805ef09c
muhfarrasm <muhammadfarrasm@gmail.com>
2023-03-23 11:07:57 +0700

    Procedur insertionsort

--- Insertion sort/Insertion sort.cpp
+++ Insertion sort/Insertion sort.cpp
@@ -22,10 +22,33 @@ void input() {					// procedure input
 	cout << "================================================" << endl;		// Membuat tampilan susunan data element array
 	cout << "===========Masukkan Element Array===============" << endl;
 	cout << "================================================" << endl;
-
+	
 	for (int i = 0; i < n; i++)										// Menggunakan perulangan for untuk menyimpan  data pada array
 	{
 		cout << "Data ke-" << (i + 1) << ": ";						
 		cin >> arr[i];												// Menyimpan nilai data n kedalam array arr
 	}
+}
+
+void insertionsort(){												// Procedur insertionsort
+	int temp;
+	int j;
+
+	for (int i = 1; i < n; i++) {			// Looping dengan i dimulai dari 1 hingga n-1
+		temp = arr[i];						// simpan nilai arr[i] ke variable sementara temp
+		j = i - 1;							// Setting nilai j sama engan i-1
+
+		while (j >= 0 && arr[j] > temp)		//Looping while dimana nilai j lebih besar sama dengan 0 dan arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];			// simpan arr[j] kedalam variable arr[j+1
+			j--;							// decrement nilai j by 1
+		}
+		arr[j + 1] = temp;					// simpan nilai temp ke dalam arr[j+1]
+
+		cout << "\nStep" << i << ": ";		// Output ke layar 
+		for (int k = 0; k < n; k++) {		// Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << "";			// Output layar
+		}
+	}
+
 }
